f3b7952e56fb6d71a60037b1631379acbe850bee
torof <scottpanam@protonmail.com>
2025-01-07 21:01:58 +0100

    refactor: simplify meta-transaction context implementation
    - Remove unnecessary IMetaTransactionContext interface
    - Make MetaTransactionContext inherit from Context
    - Update _msgSender implementation for proper inheritance

--- contracts/src/meta-transactions/MetaTransactionContext.sol
+++ contracts/src/meta-transactions/MetaTransactionContext.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import "../interfaces/IMetaTransactionContext.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
 /**
  * @title MetaTransactionContext
- * @notice Simple context helper for meta-transactions
+ * @notice Abstract contract providing context helpers for meta-transactions
+ * @dev Implements internal _msgSender to support gasless transactions
  */
-abstract contract MetaTransactionContext is IMetaTransactionContext {
-    function _msgSender() external view virtual override returns (address) {
+abstract contract MetaTransactionContext is Context {
+    /**
+     * @dev Returns the sender of the transaction (original signer for meta-transactions)
+     */
+    function _msgSender() internal view virtual override returns (address) {
         if (msg.data.length >= 20) {
             assembly {
                 let length := calldatasize()
